d93b9d6967615001f5200df3d4f097fd5d45790b
Maciej Pyrc <maciekpyrc@gmail.com>
2023-01-25 15:28:50 +0100

    feat: 🎸 Bundling adjustments

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,6 @@ import pkg from "./package.json";
 import external from "rollup-plugin-peer-deps-external";
 import del from "rollup-plugin-delete";
 import dts from "rollup-plugin-dts";
-import copy from "rollup-plugin-copy";
 
 import esbuild from "rollup-plugin-esbuild";
 
@@ -10,23 +9,10 @@ export default [
   {
     input: pkg.source,
     output: [
-      // { file: pkg.main, format: "cjs", exports: "named", sourcemap: true },
+      { file: pkg.main, format: "cjs", exports: "named", sourcemap: true },
       { file: pkg.module, format: "esm", exports: "named", sourcemap: true },
     ],
-    plugins: [
-      del({ targets: ["dist/*"] }),
-      // Todo make dynamic read from code
-      copy({
-        targets: [
-          {
-            src: ["node_modules/@docusaurus/plugin-content-docs/lib/*", "!**/*.ts"],
-            dest: "dist/lib",
-          },
-        ],
-      }),
-      external(),
-      esbuild(),
-    ],
+    plugins: [del({ targets: ["dist/*"] }), external(), esbuild()],
   },
   {
     input: pkg.source,
